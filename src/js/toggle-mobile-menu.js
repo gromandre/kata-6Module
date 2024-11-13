444585e8dd1ed34860cb5ee5b9679b91f8fe70f4
Gromov Andrew <gromov7397@yandex.ru>
2024-11-13 20:33:23 +0300

    сверстал и подключил форму обратной связи

--- src/js/toggle-mobile-menu.js
+++ src/js/toggle-mobile-menu.js
@@ -1,18 +1,31 @@
 const openButton = document.querySelector('.header__button--burger-open')
-const closeButton = document.querySelector('.action-menu__button--close')
+const closeButton = document.querySelector('#button-close-menu')
+
+const messageButtonOpen = document.querySelector(
+  '.action-menu__button--message'
+)
+const buttonCloseFeedback = document.querySelector('#buttonCloseFeedback')
+const feedback = document.querySelector('.feedback')
+
 const burgerMenu = document.querySelector('.burger-menu')
 const body = document.querySelector('body')
 const headerInner = document.querySelector('.header__inner')
 const main = document.querySelector('.main')
 const header = document.querySelector('.header') // Получаем элемент header
 
-function toggleMenu(isOpen) {
-  burgerMenu.style.display = isOpen ? 'flex' : 'none'
+function toggleMenu(isOpen, elem) {
+  elem.style.display = isOpen ? 'flex' : 'none'
   body.style.overflow = isOpen ? 'hidden' : 'auto'
   headerInner.style.opacity = isOpen ? '0.03' : '1'
   main.style.opacity = isOpen ? '0.03' : '1'
   header.style.borderBottom = isOpen ? 'none' : '1px solid #d9fff5'
 }
 
-openButton.addEventListener('click', () => toggleMenu(true))
-closeButton.addEventListener('click', () => toggleMenu(false))
+openButton.addEventListener('click', () => toggleMenu(true, burgerMenu))
+closeButton.addEventListener('click', () => toggleMenu(false, burgerMenu))
+
+messageButtonOpen.addEventListener('click', () => {
+  toggleMenu(false, burgerMenu)
+  toggleMenu(true, feedback)
+})
+buttonCloseFeedback.addEventListener('click', () => toggleMenu(false, feedback))
