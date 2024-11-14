21134d00ce9d39766f09c808baba97f82ec01100
Gromov Andrew <gromov7397@yandex.ru>
2024-11-14 13:11:12 +0300

    готово

--- src/js/toggle-mobile-menu.js
+++ src/js/toggle-mobile-menu.js
@@ -1,31 +1,67 @@
+// Элементы для мобильного меню
 const openButton = document.querySelector('.header__button--burger-open')
 const closeButton = document.querySelector('#button-close-menu')
-
-const messageButtonOpen = document.querySelector(
-  '.action-menu__button--message'
-)
-const buttonCloseFeedback = document.querySelector('#buttonCloseFeedback')
-const feedback = document.querySelector('.feedback')
-
 const burgerMenu = document.querySelector('.burger-menu')
-const body = document.querySelector('body')
-const headerInner = document.querySelector('.header__inner')
-const main = document.querySelector('.main')
-const header = document.querySelector('.header') // Получаем элемент header
+const body = document.body
+const fill = document.querySelector('.backgroun-fill')
 
+// Функция переключения меню
 function toggleMenu(isOpen, elem) {
   elem.style.display = isOpen ? 'flex' : 'none'
   body.style.overflow = isOpen ? 'hidden' : 'auto'
-  headerInner.style.opacity = isOpen ? '0.03' : '1'
-  main.style.opacity = isOpen ? '0.03' : '1'
-  header.style.borderBottom = isOpen ? 'none' : '1px solid #d9fff5'
+  fill.style.display = isOpen ? 'block' : 'none'
 }
 
-openButton.addEventListener('click', () => toggleMenu(true, burgerMenu))
-closeButton.addEventListener('click', () => toggleMenu(false, burgerMenu))
+// Открытие и закрытие мобильного меню
+if (openButton) {
+  openButton.addEventListener('click', () => toggleMenu(true, burgerMenu))
+}
+if (closeButton) {
+  closeButton.addEventListener('click', () => toggleMenu(false, burgerMenu))
+}
 
-messageButtonOpen.addEventListener('click', () => {
-  toggleMenu(false, burgerMenu)
-  toggleMenu(true, feedback)
+// Открытие формы обратной связи
+const messageButtonsOpen = document.querySelectorAll(
+  '.action-menu__button--message'
+)
+const feedback = document.querySelector('#feedback')
+messageButtonsOpen.forEach((button) => {
+  button.addEventListener('click', () => {
+    toggleMenu(false, burgerMenu)
+    toggleMenu(true, feedback)
+  })
 })
-buttonCloseFeedback.addEventListener('click', () => toggleMenu(false, feedback))
+
+// Открытие формы обратного звонка
+const callButtonsOpen = document.querySelectorAll('.action-menu__button--call')
+const modalCall = document.querySelector('#modal-call')
+callButtonsOpen.forEach((button) => {
+  button.addEventListener('click', () => {
+    toggleMenu(false, burgerMenu)
+    toggleMenu(true, modalCall)
+  })
+})
+
+// Закрытие формы обратной связи и обратного звонка
+
+// Функция для закрытия форм и восстановления стиля хедера
+function closeFormsAndRestoreHeader() {
+  toggleMenu(false, feedback) // Закрытие формы обратной связи
+  toggleMenu(false, modalCall) // Закрытие формы обратного звонка
+  toggleMenu(false, burgerMenu)
+}
+
+const buttonsCloseFeedback = document.querySelectorAll(
+  '.action-menu__button--close'
+)
+buttonsCloseFeedback.forEach((button) => {
+  button.addEventListener('click', closeFormsAndRestoreHeader)
+})
+
+// Слушаем клик на main
+fill.addEventListener('click', () => {
+  closeFormsAndRestoreHeader()
+  // closeFormsAndRestoreHeader()
+  // if (modalCall.style.display === 'flex' || feedback.style.display === 'flex')
+  //   closeFormsAndRestoreHeader()
+})
